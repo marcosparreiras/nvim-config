9370a99a603a92774910ae6df4fa45445fb42b50
Marcos Parreiras <marcos.parreiras@seidor.com>
2025-08-29 21:14:17 -0300

    plugin: add baleia to ansi colors

--- init.lua
+++ init.lua
@@ -1030,6 +1030,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.render-markdown',
+  require 'custom.plugins.ansi-colors',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
